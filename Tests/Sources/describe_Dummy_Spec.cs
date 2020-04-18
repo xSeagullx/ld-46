f6de3716309a05576e7b454e409c84931374d167
Pavel Dionisev <xSeagullx@gmail.com>
2020-04-18 11:44:30 +0100

    Initial commit

---
+++ Tests/Sources/describe_Dummy_Spec.cs
@@ -0,0 +1,17 @@
+using NSpec;
+
+namespace Tests.Sources {
+public class describe_WinSystem_Spec : nspec {
+  void when_Startup() {
+    before = () => {
+
+    };
+    it["always passes"] = () => {
+      // Given
+      // When
+      // Then
+      true.should_be(true);
+    };
+  }
+}
+}
